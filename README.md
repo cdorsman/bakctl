790a41f645a2f42142a086a91404a4e17a3dcfcf
Chris Dorsman <c.dorsman@gmail.com>
2025-03-26 14:32:55 +0000

    Expanded added subcommands and arguments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # bakctl
-bakctl is a interface for backuping MySQL database and Wordpress installation 
+bakctl is a interface for backuping MySQL or MariaDB database and Wordpress installation 
 
 # Installation
 Install the prerequisites
@@ -13,5 +13,33 @@ Execute the following command to install bakctl into /usr/local/bin
 
 # Usage
 
-For backing up Wordpress use the following command
-`
+There are two action modes: database and wordpress. To define a mode, you can use the
+--action option.
+
+Within the modes it is possible to define the following:
+
+database:
+- host: Database host. 
+- port: database port. 
+- db: Name of the database
+- username: Username to log into the database
+- tmp: Temporary directory to place the dump before transfer. Default is /tmp
+- dest: Destination directory
+
+wordpress:
+- host: Wordpress host. 
+- port: SSH port 
+- username: Username to log into the server
+- src: Source directory of the Wordpress installation 
+- dest: Destination directory
+
+
+# Examples
+
+To backup a Wordpress installation on a local server to external backup server and ask for password:
+`bakctl --action wordpress -H hostname or ip -P 22 -u user_name -p -s /var/www/wordpress -d /backup/dir`
+
+To backup a database on a local server to external backup server and ask for password:
+`bakctl --action database -H hostname or ip -P 3306  -u user_name -p -s /var/www/wordpress -d /backup/dir`
+
+For security reasons it is not possible to 
--- bakctl.py
+++ bakctl.py
@@ -2,14 +2,14 @@
 """
 Dit script is voor het maken van backups van:
 - Wordpress installatie
-- MySQL database dmv mysqldump
+- MySQL database dmv dbdump
 """
 
 __version__ = '0.1'
 __author__ = 'Chris Dorsmam'
 
 import argparse
-from sys import exit
+from sys import exit, argv
 from datetime import datetime
 import os
 import fnmatch
@@ -24,29 +24,48 @@ from scp import SCPClient
 
 logger = logging.getLogger(__name__)
 
-date = datetime.today().strftime('%Y%m%d%H%M%S')
-files = {}
+date: str = datetime.today().strftime('%Y%m%d%H%M%S')
+files: dict = {}
+dbdump_opts: list = []
+
+
+def get_db_client():
+    """
+    Function is to find a database client
+
+    Clients can be: mysqldump or mariadb-dump
+    """
+    try:
+        return which("mysqldump") or which("mariadb-dump")
+    # If database cannot be found throw exception and exit
+    except ValueError as ve:
+        logging.error("Cannot find MariaDB or MySQL client: %s", ve)
+        exit(1)
+
 
 class Bakctl:
     def init(
-            self, 
-            host: str = "", 
-            port: int = 0, 
-            username: str = "", 
+            self,
+            host: str = "",
+            port: int = 0,
+            username: str = "",
             password: str = "",
+            database: str = "",
+            tmp_dir: str = "",
             source: str = "",
-            destionation: str = ""
-        ):
-        self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
-        self.source = source
-        self.destination = destination
-        self.mysqldump = which("mysqldump")
+            destination: str = ""):
+        self.host: str = host
+        self.port: int = port
+        self.username: str = username
+        self.password: str = password
+        self.source: str = source
+        self.tmp_dir: str = tmp_dir
+        self.destination: str = destination
+        self.database: str = database
+        self.dbdump_exec: str = get_db_client()
 
-    def createSSHClient(self):
-        client = paramiko.SSHClient()
+    def createSSHClient(self) -> object:
+        client: object = paramiko.SSHClient()
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(self.host, self.port, self.username, self.password)
@@ -54,14 +73,18 @@ class Bakctl:
 
     def create_db_backup(self):
         """
-        Function initializes mysqldump to create db dumps.
+        Function initializes mysqldump or mariadb-dump to create db dumps.
 
         """
         try:
             # Calling mysql_dump with subprocess module with
             # arguments given by user.
+
+            dbdump: str = join(self.dbdump_exec, dbdump_opts)
+            cmd = f"{dbdump} > {self.tmp_dir}"
+
             subprocess.run(
-                mysql_dump_exec,
+                cmd,
                 shell=True,
                 capture_output=True
                 )
@@ -70,7 +93,6 @@ class Bakctl:
             logger.error("Exitting")
             exit(1)
 
-
     def calculate_hash(self, filename: str = None) -> hash:
         """
         Function calculating the filehash. With the filehash it is
@@ -78,7 +100,7 @@ class Bakctl:
         """
 
         # Create hash object
-        hash_object = hashlib.sha256()
+        hash_object: object = hashlib.sha256()
 
         # Open the file in binary mode
         with open(filename, 'rb') as file:
@@ -91,15 +113,13 @@ class Bakctl:
         # Return the hexadecimal representation of the hash
         return hash_object.hexdigest()
 
-
     def verify_hash(self, filename: str = None,
-        filehash_src: hash = None,
-        filehash_dest: hash = None) -> bool:
+                    filehash_src: hash = None,
+                    filehash_dest: hash = None) -> bool:
         """
         Function for checking the hash of copied file.
         """
-        return True
-
+        ...
 
     def create_wp_backup(self, src: str = None, dest: str = None) -> bool:
         """
@@ -114,16 +134,14 @@ class Bakctl:
             for root, _, files in os.walk(src):
                 for filename in filter(files, '*'):
                     filepath = os.path.join(root, filename)
-                    results[filepath] = calculate_hash(filepath)
-                    
-            shutil.copytree(src, dest)
+                    results[filepath] = self.calculate_hash(filepath)
+
+            copytree(src, dest)
 
             for root, _, files in os.walk(dest):
                 for filename in filter(files, '*'):
                     filepath = os.path.join(root, filename)
-                    results[filepath] = calculate_hash(filepath)
-            
-
+                    results[filepath] = self.calculate_hash(filepath)
             return True
 
         # If error throw exception and print error message
@@ -133,12 +151,15 @@ class Bakctl:
 
 
 def main():
+    backup = Bakctl(
+            cli.host,
+            port,
+            )
     if cli.action == 'database':
-        backup = Bakctl()
-        create_db_backup()
+        backup.create_db_backup()
 
     else:
-        create_wp_backup(src, dest)
+        backup.create_wp_backup(src, dest)
 
 
 if __name__ == '__main__':
@@ -151,75 +172,107 @@ if __name__ == '__main__':
             )
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--action",
-                        choices=["database", "wordpress"],
-                        help="Backup MySQL database or Wordpress"
-                        )
+    subparsers = parser.add_subparsers(dest='command')
 
-    parser.add_argument("-D", "--db",
-                        help="MySQL/MariaDB database to backup",
-                        default='wordpress'
-                        )
+    # Option definitions for subparser DB
+    parser_db = subparsers.add_parser("db")
+    parser_db.add_argument('-D', '--db',
+                           help='MySQL/MariaDB database to backup',
+                           default='wordpress')
 
-    parser.add_argument("-u", "--username",
-                        help="Username to log in",
-                        type=str
-                        )
+    parser_db.add_argument('-u', '--user',
+                           help='Username to log in',
+                           type=str)
 
-    parser.add_argument("-P", "--port",
-                        help="Port to conect to",
-                        type=int,
-                        )
+    parser_db.add_argument('-P', '--port',
+                           help='Port to conect to',
+                           default=3306,
+                           type=int)
 
-    parser.add_argument("-H", "--host",
-                        help="Hostname to connect to",
-                        )
+    parser_db.add_argument('-H', '--dbhost',
+                           help='Hostname of database server')
 
-    parser.add_argument("-s", "--src",
-                        help="Source directory of Wordpress installation"
-                        )
+    parser_db.add_argument('-E', '--exthost',
+                           help='Hostname of backup server')
 
-    parser.add_argument("wordpress",
-                        help="Backup Wordpress",
-                        action="store_true"
-                        )
+    parser_db.add_argument('-s', '--src',
+                           help='Source directory of Wordpress installation')
 
-    parser.add_argument("-d", "--dest",
-                        help="Destination directory to place backup"
-                        )
+    parser_db.add_argument('-d', '--dest',
+                           help='Destination directory to place backup')
 
-    parser.add_argument("-p", "--password",
-                        help="Ask for password for log into database",
-                        action="store_true"
-                        )
+    parser_db.add_argument('-t', '--tmp',
+                           help='Temporary directory to place db backup')
+
+    parser_db.add_argument('-p', '--password',
+                           help='Ask for password for log into database',
+                           action='store_true')
+
+    # Option definitions for subparser WP
+    parser_wp = subparsers.add_parser('wp')
+
+    parser_wp.add_argument('-P', '--port',
+                           help='Port to conect to',
+                           default=22,
+                           type=int)
+
+    parser_wp.add_argument('-H', '--host',
+                           help='Hostname to connect to')
+
+    parser_db.add_argument('-E', '--exthost',
+                           help='Hostname of backup server')
+
+    parser_wp.add_argument('-u', '--user',
+                           help='Username to log in',
+                           type=str)
+
+    parser_wp.add_argument('-s', '--src',
+                           help='Source directory of Wordpress installation')
+
+    parser_wp.add_argument('-d', '--dest',
+                           help='Destination directory to place backup')
+
+    parser_wp.add_argument('-p', '--password',
+                           help='Ask for password for log into database',
+                           action='store_true')
 
     cli = parser.parse_args()
 
-    # Check arguments
-    if cli.action == 'database':
-        if cli.host:
-            mysql_dump_exec += " -h {0}".format(cli.host)
-        if cli.port:
-            mysql_dump_exec += " -P {0}".format(cli.port)
-        if cli.username:
-            mysql_dump_exec += " -u {0}".format(cli.username)
-        if cli.db:
-            mysql_dump_exec += " -D {0}".format(cli.db)
-        if cli.password:
-            mysql_dump_exec += " -p"
-        logger.debug('\
+    if len(argv) == 2:
+        parser.print_help()
+        # parser.print_usage() # for just the usage line
+        parser.exit(1)
+
+    try:
+        # Check arguments
+        if cli.command == 'db':
+            if cli.host:
+                dbdump_opts += " -h {0}".format(cli.host)
+            if cli.port:
+                dbdump_opts += " -P {0}".format(cli.port)
+            if cli.user:
+                dbdump_opts += " -u {0}".format(cli.user)
+            if cli.db:
+                dbdump_opts += " -D {0}".format(cli.db)
+            if cli.password:
+                dbdump_opts += " -p"
+            
+            logger.debug('\
 mysql_dump args: Host: %s, Port: %s, Username: %s, Database %s',
                      cli.host,
                      cli.port,
-                     cli.username,
+                     cli.user,
                      cli.db)
 
-    if cli.action == 'wordpress':
-        logger.debug('\
-                Copying: Source: %s Dest: %s:%d:%s',
-                    cli.src,
-                    cli.host,
-                    cli.port,
-                    cli.dest)
+        if cli.command == 'wp':
+            logger.debug('\
+Copying: Source: %s to dest: %s:%d:%s',
+                     cli.src,
+                     cli.host,
+                     cli.port,
+                     cli.dest)
+    except ValueError as cli_err:
+        print("Argument error:", cli_err)
+        exit(1)
 
-    #main()
+    main()
